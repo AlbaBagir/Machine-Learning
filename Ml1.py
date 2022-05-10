10f9d1411ab3b4ee47b7532fbe8e6b94bfd6c234
albagir99@mail.ugm.ac.id <albabagir>
2022-05-10 21:23:12 +0700

    Add accuracy for Training and Testing

--- Ml1.py
+++ Ml1.py
@@ -4,31 +4,37 @@ from sklearn.model_selection import train_test_split
 from sklearn import neural_network, naive_bayes, svm, neighbors
 from sklearn.metrics import accuracy_score
 
-#load data
+#read the data
 data = pd.read_csv('dataset/phishing.csv')
-label = data['class']
-dataset = data.drop(['class'], axis=1)
+label = data['class']   #get the label  
+dataset = data.drop(['class'], axis=1)  #exclude the label
 
-xTrain, xTest, yTrain, yTest = train_test_split(dataset, label, test_size=0.2, random_state=0)
+xTrain, xTest, yTrain, yTest = train_test_split(dataset, label, test_size=0.2, random_state=0)  #split the data into training and testing
 
 MLPClassifier = neural_network.MLPClassifier(alpha=0.00001)
 MLPClassifier.fit(xTrain, yTrain)
-neuralNetwork = MLPClassifier.predict(xTest)
-print('Neural Network: ', accuracy_score(yTest, neuralNetwork))
+trainMlp = MLPClassifier.predict(xTrain)
+print("Training data accuracy: ", accuracy_score(yTrain, trainMlp))
+mlpClassifier = MLPClassifier.predict(xTest)
+print("MLPClassifier accuracy: ", accuracy_score(yTest, mlpClassifier))
 
 gaussianNB = naive_bayes.GaussianNB()
 gaussianNB.fit(xTrain, yTrain)
-naiveBayes = gaussianNB.predict(xTest)
-print('Naive Bayes: ', accuracy_score(yTest, naiveBayes))
-
+trainGaussian = gaussianNB.predict(xTrain)
+print("Training data accuracy: ", accuracy_score(yTrain, trainGaussian))
+gaussianClassifier = gaussianNB.predict(xTest)
+print("GaussianNB accuracy: ", accuracy_score(yTest, gaussianClassifier))
 
 LinearSVC = svm.LinearSVC(C=0.01)
 LinearSVC.fit(xTrain, yTrain)
-linearSVC = LinearSVC.predict(xTest)
-print('Linear SVC: ', accuracy_score(yTest, linearSVC))
+trainLinear = LinearSVC.predict(xTrain)
+print("Training data accuracy: ", accuracy_score(yTrain, trainLinear))
+LinearSVCClassifier = LinearSVC.predict(xTest)
+print("LinearSVC accuracy: ", accuracy_score(yTest, LinearSVCClassifier))
 
-pipline = neighbors.KNeighborsClassifier(n_neighbors=5)
-pipline.fit(xTrain, yTrain)
-pipeline = pipline.predict(xTest)
-print('Pipeline: ', accuracy_score(yTest, pipeline))
-
+KNNeighbors = neighbors.KNeighborsClassifier(n_neighbors=5)
+KNNeighbors.fit(xTrain, yTrain)
+trainKNN = KNNeighbors.predict(xTrain)
+print("Training data accuracy: ", accuracy_score(yTrain, trainKNN))
+KNNClassifier = KNNeighbors.predict(xTest)
+print("KNNeighbors accuracy: ", accuracy_score(yTest, KNNClassifier))
